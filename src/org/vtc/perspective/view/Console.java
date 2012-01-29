72cfd9c5ccefc561386175be83f26c91566d6d8e
msi88 <michael_sieber@vol.at>
2012-01-29 11:26:20 +0100

    Console view implemented.

--- src/org/vtc/perspective/view/Console.java
+++ src/org/vtc/perspective/view/Console.java
@@ -16,7 +16,15 @@
  */
 package org.vtc.perspective.view;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.ViewPart;
 
 /**
@@ -25,14 +33,21 @@ import org.eclipse.ui.part.ViewPart;
  * @author Michael Sieber
  */
 public class Console extends ViewPart {
+	private Text _console;
 
 	/*
 	 * {@inheritDoc}
 	 */
 	@Override
 	public void createPartControl(Composite parent) {
-		// TODO Auto-generated method stub
+		// create the console view
+		_console = new Text(parent, SWT.V_SCROLL);
+		_console.setEditable(false);
+		_console.setBackground(parent.getDisplay().getSystemColor(
+				SWT.COLOR_WHITE));
 
+		// setup streams
+		setUp();
 	}
 
 	/*
@@ -41,6 +56,55 @@ public class Console extends ViewPart {
 	@Override
 	public void setFocus() {
 		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Set up the console streams.
+	 */
+	private void setUp() {
+		PrintStream outStream =
+				new PrintStream(new ConsoleStream(new ByteArrayOutputStream()));
+
+		// configure System.out and System.err
+		System.setOut(outStream);
+		System.setErr(outStream);
+	}
+
+	/**
+	 * The ConsoleStream which prints the console output to the console window.
+	 * 
+	 * @author Michael Sieber
+	 */
+	private class ConsoleStream extends FilterOutputStream {
+
+		/**
+		 * Create a new ConsoleStream.
+		 * 
+		 * @param stream The output stream
+		 */
+		public ConsoleStream(OutputStream stream) {
+			super(stream);
+		}
+
+		/*
+		 * {@inheritDoc}
+		 */
+		@Override
+		public void write(byte[] b) throws IOException {
+			write(b, 0, b.length);
+		}
+
+		/*
+		 * {@inheritDoc}
+		 */
+		@Override
+		public void write(byte[] b, int off, int len) throws IOException {
+
+			// get the string and append it to the console view
+			String out = new String(b, off, len);
+			_console.append(out);
+		}
 
 	}
 }
